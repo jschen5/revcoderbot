9d3a8e83e65b8ca0a235f107092415a0314c51e9
Jason Chen <jschen5@rev.com>
2016-10-20 11:40:56 -0700

    add app id and pw

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
