574d0222f94a2c25bbe83178d7f9cea8a84580fc
Jason Chen <jschen5@rev.com>
2016-10-20 13:46:08 -0700

    fix code

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-var bot = new builder.UniversalBot(connector);
-server.post('/', connector.listen());
-
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
